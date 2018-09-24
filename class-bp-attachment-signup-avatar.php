4810dd439e08ad90596c21047b8d4a628d44031c
Brajesh Singh <brajesh@cosmiccoders.com>
2018-09-25 03:50:17 +0530

    Cleanup BP_Attachment_Signup_Avatar to make it follow wpcs

--- class-bp-attachment-signup-avatar.php
+++ class-bp-attachment-signup-avatar.php
@@ -3,122 +3,150 @@ defined( 'ABSPATH' ) || exit;
 /**
  * Contributed by Chandan Chaudhary(@ckchaudhary)
  */
-if( class_exists( '\BP_Attachment' ) ){
-    /**
-     * This is a copy of class BP_Attachment_Avatar
-     * with all the non-necessary bits stripped.
-     * All the changes are there in the function 'crop'.
-     * Other functions are copied over as is, and are just to setup the class object properly.
-     */
-    class BP_Attachment_Signup_Avatar extends \BP_Attachment  {
-        public function __construct() {
-            // Allowed avatar types.
-            $allowed_types = bp_core_get_allowed_avatar_types();
+if ( class_exists( '\BP_Attachment' ) ) {
+	/**
+	 * This is a copy of class BP_Attachment_Avatar
+	 * with all the non-necessary bits stripped.
+	 * All the changes are there in the function 'crop'.
+	 * Other functions are copied over as is, and are just to setup the class object properly.
+	 */
+	class BP_Attachment_Signup_Avatar extends \BP_Attachment {
 
-            parent::__construct( array(
-                'action'                => 'bp_avatar_upload',
-                'file_input'            => 'file',
-                'original_max_filesize' => bp_core_avatar_original_max_filesize(),
+		/**
+		 * BP_Attachment_Signup_Avatar constructor.
+		 */
+		public function __construct() {
+			// Allowed avatar types.
+			$allowed_types = bp_core_get_allowed_avatar_types();
 
-                // Specific errors for avatars.
-                'upload_error_strings'  => array(
-                    9  => sprintf( __( 'That photo is too big. Please upload one smaller than %s', 'buddypress' ), size_format( bp_core_avatar_original_max_filesize() ) ),
-                    10 => sprintf( _n( 'Please upload only this file type: %s.', 'Please upload only these file types: %s.', count( $allowed_types ), 'buddypress' ), self::get_avatar_types( $allowed_types ) ),
-                ),
-            ) );
-        }
-        
-        public static function get_avatar_types( $allowed_types = array() ) {
-            $types = array_map( 'strtoupper', $allowed_types );
-            $comma = _x( ',', 'avatar types separator', 'buddypress' );
-            return join( $comma . ' ', $types );
-        }
-        
-        public function set_upload_dir() {
-            if ( bp_core_avatar_upload_path() && bp_core_avatar_url() ) {
-                $this->upload_path = bp_core_avatar_upload_path();
-                $this->url         = bp_core_avatar_url();
-                $this->upload_dir  = bp_upload_dir();
-            } else {
-                parent::set_upload_dir();
-            }
-        }
-        
-        public function crop( $args = array() ) {
-            // Bail if the original file is missing.
-            if ( empty( $args['original_file'] ) ) {
-                return false;
-            }
-            
-            if ( ! bp_attachments_current_user_can( 'edit_avatar', $args ) ) {
-                return false;
-            }
+			parent::__construct( array(
+				'action'                => 'bp_avatar_upload',
+				'file_input'            => 'file',
+				'original_max_filesize' => bp_core_avatar_original_max_filesize(),
 
-            $avatar_dir = 'avatars';
+				// Specific errors for avatars.
+				'upload_error_strings'  => array(
+					/* translators: Maximum allowed avatar file size */
+					9  => sprintf( __( 'That photo is too big. Please upload one smaller than %s', 'buddypress' ), size_format( bp_core_avatar_original_max_filesize() ) ),
+					/* translators: Allowed file type */
+					10 => sprintf( _n( 'Please upload only this file type: %s.', 'Please upload only these file types: %s.', count( $allowed_types ), 'buddypress' ), self::get_avatar_types( $allowed_types ) ),
+				),
+			) );
+		}
 
-            /**
-             * Original file is a relative path to the image
-             * eg: /avatars/1/avatar.jpg
-             */
-            $relative_path = sprintf( '/%s/%s/%s', $avatar_dir, $args['item_id'], basename( $args['original_file'] ) );
-            $absolute_path = $this->upload_path . $relative_path;
-            
-            // Bail if the avatar is not available.
-            if ( ! file_exists( $absolute_path ) )  {
-                return false;
-            }
+		/**
+		 * Get allowed avatar image type as string.
+		 *
+		 * @param array $allowed_types extensions.
+		 *
+		 * @return string
+		 */
+		public static function get_avatar_types( $allowed_types = array() ) {
+			$types = array_map( 'strtoupper', $allowed_types );
+			$comma = _x( ',', 'avatar types separator', 'buddypress' );
 
-            if ( empty( $args['item_id'] ) ) {
+			return join( $comma . ' ', $types );
+		}
 
-                /** This filter is documented in bp-core/bp-core-avatars.php */
-                $avatar_folder_dir = apply_filters( 'bp_core_avatar_folder_dir', dirname( $absolute_path ), $args['item_id'], $args['object'], $args['avatar_dir'] );
-            } else {
+		/**
+		 * Set upload directory.
+		 */
+		public function set_upload_dir() {
+			if ( bp_core_avatar_upload_path() && bp_core_avatar_url() ) {
+				$this->upload_path = bp_core_avatar_upload_path();
+				$this->url         = bp_core_avatar_url();
+				$this->upload_dir  = bp_upload_dir();
+			} else {
+				parent::set_upload_dir();
+			}
+		}
 
-                /** This filter is documented in bp-core/bp-core-avatars.php */
-                $avatar_folder_dir = apply_filters( 'bp_core_avatar_folder_dir', $this->upload_path . '/' . $args['avatar_dir'] . '/' . $args['item_id'], $args['item_id'], $args['object'], $args['avatar_dir'] );
-            }
+		/**
+		 * Crop image.
+		 *
+		 * @param array $args args.
+		 *
+		 * @return array|bool|string|WP_Error
+		 */
+		public function crop( $args = array() ) {
+			// Bail if the original file is missing.
+			if ( empty( $args['original_file'] ) ) {
+				return false;
+			}
 
-            // Bail if the avatar folder is missing for this item_id.
-            if ( ! file_exists( $avatar_folder_dir ) ) {
-                return false;
-            }
-            
-            // Make sure we at least have minimal data for cropping.
-            if ( empty( $args['crop_w'] ) ) {
-                $args['crop_w'] = bp_core_avatar_full_width();
-            }
+			if ( ! bp_attachments_current_user_can( 'edit_avatar', $args ) ) {
+				return false;
+			}
 
-            if ( empty( $args['crop_h'] ) ) {
-                $args['crop_h'] = bp_core_avatar_full_height();
-            }
+			$avatar_dir = 'avatars';
 
-            // Get the file extension.
-            $data = @getimagesize( $absolute_path );
-            $ext  = $data['mime'] == 'image/png' ? 'png' : 'jpg';
+			/**
+			 * Original file is a relative path to the image
+			 * eg: /avatars/1/avatar.jpg
+			 */
+			$relative_path = sprintf( '/%s/%s/%s', $avatar_dir, $args['item_id'], basename( $args['original_file'] ) );
+			$absolute_path = $this->upload_path . $relative_path;
 
-            $args['original_file'] = $absolute_path;
-            $args['src_abs']       = false;
-            $avatar_types = array( 'full' => '', 'thumb' => '' );
+			// Bail if the avatar is not available.
+			if ( ! file_exists( $absolute_path ) ) {
+				return false;
+			}
 
-            foreach ( $avatar_types as $key_type => $type ) {
-                if ( 'thumb' === $key_type ) {
-                    $args['dst_w'] = bp_core_avatar_thumb_width();
-                    $args['dst_h'] = bp_core_avatar_thumb_height();
-                } else {
-                    $args['dst_w'] = bp_core_avatar_full_width();
-                    $args['dst_h'] = bp_core_avatar_full_height();
-                }
+			if ( empty( $args['item_id'] ) ) {
 
-                $filename         = wp_unique_filename( $avatar_folder_dir, uniqid() . "-bp{$key_type}.{$ext}" );
-                $args['dst_file'] = $avatar_folder_dir . '/' . $filename;
+				/** This filter is documented in bp-core/bp-core-avatars.php */
+				$avatar_folder_dir = apply_filters( 'bp_core_avatar_folder_dir', dirname( $absolute_path ), $args['item_id'], $args['object'], $args['avatar_dir'] );
+			} else {
 
-                $avatar_types[ $key_type ] = parent::crop( $args );
-            }
-            // Remove the original.
-            @unlink( $absolute_path );
+				/** This filter is documented in bp-core/bp-core-avatars.php */
+				$avatar_folder_dir = apply_filters( 'bp_core_avatar_folder_dir', $this->upload_path . '/' . $args['avatar_dir'] . '/' . $args['item_id'], $args['item_id'], $args['object'], $args['avatar_dir'] );
+			}
 
-            return $avatar_types;
-        }
+			// Bail if the avatar folder is missing for this item_id.
+			if ( ! file_exists( $avatar_folder_dir ) ) {
+				return false;
+			}
 
-    }
+			// Make sure we at least have minimal data for cropping.
+			if ( empty( $args['crop_w'] ) ) {
+				$args['crop_w'] = bp_core_avatar_full_width();
+			}
+
+			if ( empty( $args['crop_h'] ) ) {
+				$args['crop_h'] = bp_core_avatar_full_height();
+			}
+
+			// Get the file extension.
+			$data = @getimagesize( $absolute_path );
+			$ext  = 'image/png' === $data['mime'] ? 'png' : 'jpg';
+
+			$args['original_file'] = $absolute_path;
+			$args['src_abs']       = false;
+
+			$avatar_types = array(
+				'full'  => '',
+				'thumb' => '',
+			);
+
+			foreach ( $avatar_types as $key_type => $type ) {
+				if ( 'thumb' === $key_type ) {
+					$args['dst_w'] = bp_core_avatar_thumb_width();
+					$args['dst_h'] = bp_core_avatar_thumb_height();
+				} else {
+					$args['dst_w'] = bp_core_avatar_full_width();
+					$args['dst_h'] = bp_core_avatar_full_height();
+				}
+
+				$filename         = wp_unique_filename( $avatar_folder_dir, uniqid() . "-bp{$key_type}.{$ext}" );
+				$args['dst_file'] = $avatar_folder_dir . '/' . $filename;
+
+				$avatar_types[ $key_type ] = parent::crop( $args );
+			}
+			// Remove the original.
+			@unlink( $absolute_path );
+
+			return $avatar_types;
+		}
+
+	}
 }
